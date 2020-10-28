7e692af0be73b4928209111d28a9ab1c43ca8412
Rahul00641 <thakurji296@gmail.com>
2020-10-28 10:15:11 +0530

    Count Sort Algorithm, It only works with positive Integers

--- CountSort.java
+++ CountSort.java
@@ -1,23 +1,33 @@
-package MyPackage;
+package MyPackage.Algorithms;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class CountSort {
 
     public static void main(String[] args){
 
-        int[] ar = new int[]{2,3,1,5,3,5,3,2,5,7,9,6,3,5,3};
-        int[] count = new int[9+1];
-        for(int i=0;i<ar.length;i++){
+        Scanner in =new Scanner(System.in);
+        int size = in.nextInt();
+        int[] ar = new int[size];
+        int maxElement = 0 ;
+        for(int i=0;i<size;i++){
+            ar[i] = in.nextInt();
+            if(ar[i]>maxElement)maxElement=ar[i];
+        }
+        int[] count = new int[maxElement+1];
+        for(int i=0;i<size;i++){
             count[ar[i]]++;
         }
-        for(int i=1;i< count.length;i++){
+        for(int i=1;i< maxElement+1;i++){
             count[i]+=count[i-1];
         }
-        int[] result = new int[ar.length];
-        for(int i=ar.length-1;i>=0;i--){
+        int[] result = new int[size];
+        for(int i=size-1;i>=0;i--){
             count[ar[i]]--;
             result[count[ar[i]]]=ar[i];
         }
-        for(int i=0;i<ar.length;i++){
+        for(int i=0;i<size;i++){
             System.out.print(result[i]+" ");
         }
 
